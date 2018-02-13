22461b4e5429dd9b37736d9b170c4d6633d6540c
nataniohan <natan.teodoro@hotmail.com>
2018-02-12 22:27:48 -0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int otherNumber = 0; 
+		int otherNumber; 
 		int all=0;
 		int[] numero = { 12, 13, 8, 8, 5, 5, 4 };
-		for (int i = 0; i <numero.length; i++) {
-			otherNumber++;
-		}
+		otherNumber = numero.length;
 		for (int i = 0; i <numero.length; i++){
 			for (int x = 0; x <numero.length; x++) {
 				if (numero[i] == numero[x]) {
